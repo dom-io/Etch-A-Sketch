ae209db30579c4dad8c9064c200470427280e8d5
Dom <111844962+dom-io@users.noreply.github.com>
2022-11-16 13:09:07 -0800

    The etch-a-sketchpad is fully complete with a working color selection, a buton to change to that selected color, a button for rainbow color, an eraser button, and a reset button. There is also a slider that dynamically changes the size and amount of pixels.

--- app.js
+++ app.js
@@ -1,34 +1,87 @@
-// const grid = document.createElement('grid');
-// grid.style.width = '100px';
-// grid.style.height = '200px';
-// grid.style.background = 'blue';
-// grid.style.color = 'white';
-// grid.innerHTML = 'Hello world';
-// grid.style.paddingRight = '50px';
+const container = document.querySelector('.container');
+const color = document.querySelector('.color');
+const sizeSlider = document.querySelector('.size-slider');
+const resetbtn = document.querySelector('.reset-btn');
+const clearbtn = document.querySelector('.clear-btn');
+const colorbtn = document.querySelector('.color-btn');
+const eraserbtn = document.querySelector('.eraser-btn');
+const rainbowBtn = document.querySelector('.rainbow-btn');
+const sizeSelection = document.querySelector('.size-selection');
+// const removeGridlines = document.querySelector('.remove-gridlines');
+let size = sizeSlider.value;
+let currentColor = 'color';
 
-// document.body.appendChild(grid).container;
+let btnPick = true;
+document.body.onmousedown = () => (btnPick = true);
+document.body.onmouseup = () => (btnPick = false);
 
-const container = document.querySelector('#container');
-let row = container.appendChild(document.createElement('div'));
-row.className = 'row';
-let box = document.createElement('div');
-box.className = 'box';
-const color = document.querySelector('box');
+colorbtn.onmouseover = () => colorStyle('color');
+rainbowBtn.onmousedown = () => colorStyle('rainbow');
+eraserbtn.onmousedown = () => colorStyle('eraser');
+resetbtn.onmousedown = () => resetSketch(size);
+// clearbtn.onmousedown = () => clearBtnWipe(size);
+// removeGridlines.onclick = () => gridlines();
 
-function createGrid(boxAmount) {
-  for (let i = 0; i < boxAmount; i++) {
-    let row = container.appendChild(document.createElement('div'));
-    row.className = 'row';
-    for (let j = 0; j < boxAmount; j++) {
-      let box = document.createElement('div');
-      box.className = 'box';
-      row.appendChild(box);
-    }
+sizeSlider.onmousemove = (e) => changeSize(e.target.value);
+sizeSlider.onchange = (e) => resetPixels(e.target.value);
+
+function changeSize(size) {
+  sizeSelection.innerHTML = `${size} x ${size}`;
+}
+
+// function gridlines() {
+//   container.style.border = 'none';
+// }
+
+// function clearBtnWipe(size) {
+//   resetPixels(size);
+//   sizeSlider.value = size;
+// }
+
+function resetPixels(size) {
+  clearpixels();
+  createGrid(size);
+}
+
+function clearpixels() {
+  container.innerHTML = '';
+}
+
+function resetSketch(size) {
+  resetPixels(size);
+  sizeSlider.value = 12;
+  sizeSelection.textContent = '12 x 12';
+}
+
+function colorStyle(newColor) {
+  currentColor = newColor;
+}
+
+function createGrid(size) {
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement('div');
+    div.classList.add('pixel');
+    div.addEventListener('mouseover', changeColor);
+    div.addEventListener('mouseup', changeColor);
+    container.appendChild(div);
   }
 }
 
-createGrid(5);
+function changeColor(event) {
+  if (event.type !== 'mouseenter' && event.type == 'mousedown') return;
+  if (currentColor == 'color') {
+    event.target.style.backgroundColor = color.value;
+  } else if (currentColor == 'rainbow') {
+    const rColor = Math.floor(Math.random() * 256);
+    const gColor = Math.floor(Math.random() * 256);
+    const bColor = Math.floor(Math.random() * 256);
+    event.target.style.backgroundColor = `rgb(${rColor}, ${gColor}, ${bColor})`;
+  } else if (currentColor == 'eraser') {
+    event.target.style.backgroundColor = 'rgba(45,45,45)';
+  }
+}
 
-box.addEventListener('mouseover', (event) => {
-  event.target.style.color = 'red';
-});
+createGrid(size);
